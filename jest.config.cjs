500ae663a31a58c562f9598ef94b9a8a0c890697
Sergey Korsik <serg.korsik@gmail.com>
2026-05-03 17:48:49 +0200

    chore: added more strict pre commit hook

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverage: process.env.CI || process.argv.includes('--coverage'),
   collectCoverageFrom: ['src/**/*.ts', '!src/cli.ts'],
   coverageThreshold: {
     global: {
@@ -10,6 +11,13 @@ module.exports = {
       functions: 90,
       lines: 80,
       statements: 80
+    },
+    // Per-file thresholds to catch individual files with poor coverage
+    './src/**/!(*.test|*.spec|cli).ts': {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60
     }
   },
   transform: {
